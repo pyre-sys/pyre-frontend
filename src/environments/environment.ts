fc2891d6e1ee3af9db5d98f93e36942753cea8c4
MenichettiDev <menichetti.dev@gmail.com>
2025-11-05 01:01:10 -0300

    Fix - Json modificacion para produccion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:2000/api', // URL para desarrollo
+  apiUrl: 'http://localhost:1000/api', // URL para desarrollo
   // apiUrl: 'http://147.93.32.147:4000/api', // URL prod
 
 };
