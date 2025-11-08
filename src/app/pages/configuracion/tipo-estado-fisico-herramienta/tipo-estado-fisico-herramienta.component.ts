e2d7476cfc220a29812f9d6e115f8b7718a5ef61
MenichettiDev <menichetti.dev@gmail.com>
2025-11-08 13:58:57 -0300

    Feat - Configuracion - Perifericos herramientas

---
+++ src/app/pages/configuracion/tipo-estado-fisico-herramienta/tipo-estado-fisico-herramienta.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tipo-estado-fisico-herramienta',
+  imports: [],
+  templateUrl: './tipo-estado-fisico-herramienta.component.html',
+  styleUrl: './tipo-estado-fisico-herramienta.component.css'
+})
+export class TipoEstadoFisicoHerramientaComponent {
+
+}
